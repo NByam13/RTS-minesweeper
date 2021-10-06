a85a1edb43e655f91184a62da16af30a84316bda
Vehikl Team <go@vehikl.com>
2021-10-06 15:52:44 -0400

    remove unneeded css rules

--- src/components/gameboard/index.tsx
+++ src/components/gameboard/index.tsx
@@ -8,17 +8,6 @@ const GameBoardGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-  gap: 0px 0px;
-  grid-template-areas:
-    ". . . . . . . . ."
-    ". . . . . . . . ."
-    ". . . . . . . . ."
-    ". . . . . . . . ."
-    ". . . . . . . . ."
-    ". . . . . . . . ."
-    ". . . . . . . . ."
-    ". . . . . . . . ."
-    ". . . . . . . . .";
 `;
 
 // write tests for next session
